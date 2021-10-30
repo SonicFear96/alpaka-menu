f6f55d6b6ad97e7914023271bc174086736553c5
Bogdan Rudakov <48532613+SonicFear96@users.noreply.github.com>
2021-10-30 22:11:26 +0300

    menu hover + submenu

--- js/script.js
+++ js/script.js
@@ -4,3 +4,6 @@ const navToggle = document.querySelector(".nav__toggle");
 navToggle.addEventListener("click", function () {
   navMain.classList.toggle("display-toggle");
 });
+
+const usedSubMenu = document.querySelector(".used-submenu");
+const firstSubMenu = document.querySelector(".nav__sub-menu");
