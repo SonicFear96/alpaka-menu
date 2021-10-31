f7b0c7068b06044a3c47204360e326d38ff633ab
Bogdan Rudakov <48532613+SonicFear96@users.noreply.github.com>
2021-10-31 17:23:50 +0300

    js event menu

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 const navMain = document.querySelector(".header__nav");
 const navToggle = document.querySelector(".nav__toggle");
 
+const usedSubMenu = document.querySelector(".used-submenu");
+const usedSubMenuSub = document.querySelector(".used-sub-submenu");
+const firstSubMenu = document.querySelector(".nav__sub-menu");
+const secondSubMenu = document.querySelector(".sub-menu__sub");
+
 navToggle.addEventListener("click", function () {
   navMain.classList.toggle("display-toggle");
 });
 
-const usedSubMenu = document.querySelector(".used-submenu");
-const firstSubMenu = document.querySelector(".nav__sub-menu");
-const secodnSubMenu = document.querySelector(".sub-menu__sub");
+usedSubMenu.addEventListener("click", function () {
+  if (document.documentElement.clientWidth < 1023) {
+    console.log("произошёл клик");
+    firstSubMenu.classList.toggle("active");
+    event.stopPropagation();
+  }
+});
+
+usedSubMenuSub.addEventListener("click", function () {
+  if (document.documentElement.clientWidth < 1023) {
+    console.log("произошёл клик подподменю");
+    secondSubMenu.classList.toggle("active");
+    event.stopPropagation();
+  }
+});
